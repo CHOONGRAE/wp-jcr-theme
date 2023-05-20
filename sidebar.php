9b71b9d47e443a1ebc019f9cff6b15e1bac94ae4
CHOONGRAE <choongraej@gmail.com>
2023-05-21 00:22:38 +0900

    Feat(template): sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<aside id="Sidebar">
+    <div class='btn-close-sidebar'>
+        <svg viewBox='0 0 30 30' width='30px' height='30px'>
+            <line x1='5' y1='5' x2='25' y2='25' stroke='#eee' stroke-width='3.5' stroke-linecap='round'
+                stroke-linejoin='round' />
+            <line x1='25' y1='5' x2='5' y2='25' stroke='#eee' stroke-width='3.5' stroke-linecap='round'
+                stroke-linejoin='round' />
+        </svg>
+    </div>
+    <div class='sidebar-items'>
+
+        <?php
+        include_once "templates/sidebar/searchform.php";
+        include_once "templates/sidebar/categories.php";
+        ?>
+
+    </div>
+</aside>
