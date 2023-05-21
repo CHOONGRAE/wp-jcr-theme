9713172d0d702aa570d62a23221f84a343c07979
CHOONGRAE <choongraej@gmail.com>
2023-05-21 16:51:10 +0900

    Feat(functions): Add functions.php
    Feat: Register main menu
    Feat: Hide admin bar

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+//메인 메뉴
+register_nav_menus(["header" => __("Header Menu")]);
+
+//관리 표시줄 숨김
+add_action("after_setup_theme", "remove_admin_bar");
+function remove_admin_bar()
+{
+  show_admin_bar(false);
+  // if (!current_user_can("administrator") && !is_admin()) {
+  //   show_admin_bar(false);
+  // }
+}
