d16f4b1dfadb474a784e32ddc1d0164df50aba10
CHOONGRAE <choongraej@gmail.com>
2023-05-24 22:46:17 +0900

    Design(editor): Add editor style
    Feat(functions): Add action for editor style

--- functions.php
+++ functions.php
@@ -5,12 +5,22 @@ register_nav_menus(["header" => __("Header Menu")]);
 
 //관리 표시줄 숨김
 add_action("after_setup_theme", "remove_admin_bar");
+
 function remove_admin_bar()
 {
   show_admin_bar(false);
   // if (!current_user_can("administrator") && !is_admin()) {
   //   show_admin_bar(false);
   // }
+}
+
+//편집기 css 적용
+add_action("after_setup_theme", "set_css_editor");
+
+function set_css_editor()
+{
+  add_theme_support("editor-styles");
+  add_editor_style(get_theme_file_uri("/styles/editor.css"));
 }
 
 //이미지 업로드 처리
